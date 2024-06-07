a73199a06edf70648ce78509a244b9a92495a98d
superderped <24651666+DipRRai@users.noreply.github.com>
2024-06-08 03:45:04 +1000

    s

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.117.1/build/three.module.js';
+import * as THREE from 'https://unpkg.com/three@0.97.0/build/three.module.js';
 
 const element = document.getElementById('displayer');
 const scene = new THREE.Scene();
